13bd606b5d85642788c4c7aeba588d2e15bca8b9
Ahmad Moradi <=>
2023-07-15 02:59:52 +0330

    chore: Create global types declaration file

---
+++ global.d.ts
@@ -0,0 +1,21 @@
+interface Article {
+  slug: string;
+  title: string;
+  description: string;
+  body: string;
+  tagList: string[];
+  createdAt: string;
+  updatedAt: string;
+  favorited: boolean;
+  favoritesCount: number;
+  author: {
+    username: string;
+    bio: null | string;
+    image: string;
+    following: boolean;
+  };
+}
+
+declare global {
+  Article;
+}
